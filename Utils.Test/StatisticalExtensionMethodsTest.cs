2499e66622219a8759457c89533f208acb37d91b
Josh Lospinoso <josh@redowlconsulting.com>
2011-09-07 05:28:22 -0400

    Committing statistical extension methods for collections

---
+++ Utils.Test/StatisticalExtensionMethodsTest.cs
@@ -0,0 +1,96 @@
+﻿using Lospi.Utils.Collections;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+
+namespace Lospi.Utils.Test
+{
+    
+    
+    /// <summary>
+    ///This is a test class for StatisticalExtensionMethodsTest and is intended
+    ///to contain all StatisticalExtensionMethodsTest Unit Tests
+    ///</summary>
+    [TestClass()]
+    public class StatisticalExtensionMethodsTest
+    {
+        double[] values = { -4D, 4D, -4D, 4D };
+
+        private TestContext testContextInstance;
+
+        /// <summary>
+        ///Gets or sets the test context which provides
+        ///information about and functionality for the current test run.
+        ///</summary>
+        public TestContext TestContext
+        {
+            get
+            {
+                return testContextInstance;
+            }
+            set
+            {
+                testContextInstance = value;
+            }
+        }
+
+        #region Additional test attributes
+        // 
+        //You can use the following additional attributes as you write your tests:
+        //
+        //Use ClassInitialize to run code before running the first test in the class
+        //[ClassInitialize()]
+        //public static void MyClassInitialize(TestContext testContext)
+        //{
+        //}
+        //
+        //Use ClassCleanup to run code after all tests in a class have run
+        //[ClassCleanup()]
+        //public static void MyClassCleanup()
+        //{
+        //}
+        //
+        //Use TestInitialize to run code before running each test
+        //[TestInitialize()]
+        //public void MyTestInitialize()
+        //{
+        //}
+        //
+        //Use TestCleanup to run code after each test has run
+        //[TestCleanup()]
+        //public void MyTestCleanup()
+        //{
+        //}
+        //
+        #endregion
+
+
+        /// <summary>
+        ///A test for StandardDeviation
+        ///</summary>
+        [TestMethod()]
+        public void StandardDeviationTest()
+        {
+            ICollection<double> x = new List<double>(values);
+            bool sample = false;
+            double expected = 2F;
+            double actual;
+            actual = StatisticalExtensionMethods.StandardDeviation(x, sample);
+            Assert.AreEqual(expected, actual);
+        }
+
+        /// <summary>
+        ///A test for Variance
+        ///</summary>
+        [TestMethod()]
+        public void VarianceTest()
+        {
+            ICollection<double> x = new List<double>(values);
+            bool sample = false;
+            double expected = 16F;
+            double actual;
+            actual = StatisticalExtensionMethods.Variance(x, sample);
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
---
+++ Utils/Collections/StatisticalExtensionMethods.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Lospi.Utils.Collections
+{
+    /// <summary>
+    /// Houses statistical extension methods for collections
+    /// </summary>
+    public static class StatisticalExtensionMethods
+    {
+        /// <summary>
+        /// Calculates the standard deviation of a collection of doubles
+        /// </summary>
+        /// <param name="x">A collection of doubles</param>
+        /// <param name="sample">True if sample standard deviation is to be calculated, false if population</param>
+        /// <returns>The standard deviation of x</returns>
+        public static Double StandardDeviation(this ICollection<Double> x, bool sample=true)
+        {
+            Double mean = x.Average();
+            Double denominator = x.Count;
+            if (sample)
+            {
+                denominator--;
+            }
+            return Math.Sqrt(x.Select(val => Math.Pow(val - mean, 2)).Sum())/denominator;
+        }
+
+        /// <summary>
+        /// Calculates the variance of a collection of doubles
+        /// </summary>
+        /// <param name="x">A collection of doubles</param>
+        /// <param name="sample">True if sample variance is to be calculated, false if population</param>
+        /// <returns>The variance of x</returns>
+        public static Double Variance(this ICollection<Double> x, bool sample = true)
+        {
+            Double mean = x.Average();
+            Double denominator = x.Count;
+            if (sample)
+            {
+                denominator--;
+            }
+            return x.Select(val => Math.Pow(val - mean, 2)).Sum() / denominator;
+        }
+    }
+}
