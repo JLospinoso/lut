9f773b7af77e2653caa37fc44cad3947ec340167
Josh Lospinoso <josh@redowlconsulting.com>
2011-10-04 11:03:00 -0400

    Adding Lib folder for continuous integration

---
+++ UtilsTest/Generics/SealedSymmetricTwoKeyDictionaryTest.cs
@@ -0,0 +1,61 @@
+﻿/*
+ * Copyright © 2011, Joshua A. Lospinoso (josh@lospi.net). All rights reserved.
+ */
+
+using NUnit.Framework;
+using Lospi.Utils.Generics;
+using System.Collections.Generic;
+
+namespace Lospi.Test.Utils.Generics
+{
+    [TestFixture]
+    public class SealedSymmetricTwoKeyDictionaryTest
+    {
+        SealedSymmetricTwoKeyDictionary<string, int> dict;
+
+        [SetUp]
+        public void TwoKeyDictionaryTestSetUp()
+        {
+            string[] keys = { "a", "b", "c"};
+
+            dict = new SealedSymmetricTwoKeyDictionary<string, int>(keys);
+
+            dict["a", "a"] = 1;
+            dict["a", "b"] = 2;
+            dict["a", "c"] = 3;
+            dict["b", "b"] = 4;
+            dict["b", "c"] = 5;
+            dict["c", "c"] = 6;
+        }
+
+        [TearDown]
+        public void TwoKeyDictionaryTestTearDown()
+        {
+            dict = null;
+        }
+
+        [Test]
+        public void TwoKeyDictionaryTest_Getters_EqualSetters()
+        {
+            Assert.That(dict["a", "a"], Is.EqualTo(1));
+            Assert.That(dict["b", "a"], Is.EqualTo(2));
+            Assert.That(dict["a", "b"], Is.EqualTo(2));
+            Assert.That(dict["a", "c"], Is.EqualTo(3));
+            Assert.That(dict["c", "a"], Is.EqualTo(3));
+            Assert.That(dict["b", "b"], Is.EqualTo(4));
+            Assert.That(dict["b", "c"], Is.EqualTo(5));
+            Assert.That(dict["c", "b"], Is.EqualTo(5));
+            Assert.That(dict["c", "c"], Is.EqualTo(6));
+        }
+
+        [Test]
+        public void TwoKeyDictionaryTest_Marginalizer_Works()
+        {
+            var result = dict["a"];
+            Assert.That(result["a"], Is.EqualTo(1));
+            Assert.That(result["b"], Is.EqualTo(2));
+            Assert.That(result["c"], Is.EqualTo(3));
+        }
+
+    }
+}
---
+++ UtilsTest/Generics/SealedTwoKeyDictionaryTest.cs
@@ -0,0 +1,62 @@
+﻿/*
+ * Copyright © 2011, Joshua A. Lospinoso (josh@lospi.net). All rights reserved.
+ */
+
+using NUnit.Framework;
+using Lospi.Utils.Generics;
+using System.Collections.Generic;
+
+namespace Lospi.Test.Utils.Generics
+{
+    [TestFixture]
+    public class SealedTwoKeyDictionaryTest
+    {
+        SealedTwoKeyDictionary<string, int, double> dict;
+
+        [SetUp]
+        public void TwoKeyDictionaryTestSetUp()
+        {
+            string[] firstKeyStrings = { "a", "b" };
+            int[] secondKeyInts = { 1, 2, 3 };
+
+            dict = new SealedTwoKeyDictionary<string, int, double>(firstKeyStrings, secondKeyInts);
+
+            dict["a", 1] = 1D;
+            dict["a", 2] = 2D;
+            dict["a", 3] = 3D;
+
+            dict["b", 1] = 4D;
+            dict["b", 2] = 5D;
+            dict["b", 3] = 6D;
+        }
+
+        [TearDown]
+        public void TwoKeyDictionaryTestTearDown()
+        {
+            dict = null;
+        }
+
+        [Test]
+        public void TwoKeyDictionaryTest_Getters_EqualSetters()
+        {
+            Assert.That(dict["b", 2], Is.EqualTo(5D));
+        }
+
+        [Test]
+        public void TwoKeyDictionaryTest_KeyOneMarginalizer_Works()
+        {
+            var result = dict.MarginalizeKeyOne("b");
+            Assert.That(result[1], Is.EqualTo(4D));
+            Assert.That(result[2], Is.EqualTo(5D));
+            Assert.That(result[3], Is.EqualTo(6D));
+        }
+
+        [Test]
+        public void TwoKeyDictionaryTest_KeyTwoMarginalizer_Works()
+        {
+            var result = dict.MarginalizeKeyTwo(3);
+            Assert.That(result["a"], Is.EqualTo(3D));
+            Assert.That(result["b"], Is.EqualTo(6D));
+        }
+    }
+}
